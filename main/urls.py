ba2991916e716e71568daf7059e9f435f68ddd1e
KnightOfMelons <ilaprosmotrov@gmail.com>
2023-09-03 20:18:46 +0400

    Added authentication and registration system

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 from main import views
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
 ]
